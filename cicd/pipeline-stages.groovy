afb3ac60c8392671205f81a9f209bdc4dbe9f8cd
Stepan Shysh <stepan.shysh@essenceglobal.com>
2019-10-08 17:47:31 +0300

    add jenkins file

--- cicd/pipeline-stages.groovy
+++
@@ -1,8 +0,0 @@
-node('docker') {
-
-    stage 'Checkout'
-        checkout scm
-    stage 'Build & UnitTest'
-        sh "docker build -t conduit:B${BUILD_NUMBER} -f Dockerfile ."
-        sh "docker build -t conduit:test-B${BUILD_NUMBER} -f Dockerfile.Integration ."
-}
