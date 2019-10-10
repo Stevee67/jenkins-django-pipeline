f3e10f60ac95db9f9c67755d238046ceee3fc5dd
Stepan Shysh <stepan.shysh@essenceglobal.com>
2019-10-10 14:55:28 +0300

    add broken unittest

--- admin-panel/admin_panel/background_tasks/tests.py
+++ admin-panel/admin_panel/background_tasks/tests.py
@@ -6,3 +6,7 @@ class AnimalTestCase(TestCase):
     def test_animals_can_speak(self):
         """Animals that can speak are correctly identified"""
         self.assertEqual(1, 1)
+
+    def test_animals_cannot_speak(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(1, 2)
