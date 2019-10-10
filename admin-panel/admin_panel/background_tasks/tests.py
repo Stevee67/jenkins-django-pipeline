e8224d86fd0a3713141312d51e60843759bbd408
Stepan Shysh <stepan.shysh@essenceglobal.com>
2019-10-10 18:06:11 +0300

    remove broken test

--- admin-panel/admin_panel/background_tasks/tests.py
+++ admin-panel/admin_panel/background_tasks/tests.py
@@ -10,7 +10,3 @@ class AnimalTestCase(TestCase):
     def test_animals_can_speak2(self):
         """Animals that can speak are correctly identified"""
         self.assertEqual(2, 2)
-
-    def test_animals_cannot_speak(self):
-        """Animals that can speak are correctly identified"""
-        self.assertEqual(1, 2)
