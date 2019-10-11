2ae9a5480df15c3861a2b8a7d1d64825b8ae9dc6
Stepan Shysh <stepan.shysh@essenceglobal.com>
2019-10-11 16:08:10 +0300

    add test

--- admin-panel/admin_panel/background_tasks/tests.py
+++ admin-panel/admin_panel/background_tasks/tests.py
@@ -10,3 +10,7 @@ class AnimalTestCase(TestCase):
     def test_animals_can_speak2(self):
         """Animals that can speak are correctly identified"""
         self.assertEqual(2, 2)
+
+    def test_animals_can_speak3(self):
+        """Animals that can speak are correctly identified"""
+        self.assertEqual(2, 2)
